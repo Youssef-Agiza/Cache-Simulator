aa65bf0c7e444cc260ed33d240c396337dca4529
Youssef Agiza <youssefagiza@aucegypt.edu>
2021-07-14 01:26:20 +0200

    Implements plotter in Pythongit aa

--- src/cacheSim.cpp
+++ src/cacheSim.cpp
@@ -1,10 +1,9 @@
 #include "../headers/Utilities.h"
-#include <iomanip>
-#include <array>
 
 int main()
 {
     HandleInput();
     //Test1();
+    // system("python3 plotter/plotter.py");
     return 0;
 }
